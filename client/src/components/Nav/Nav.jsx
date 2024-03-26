84292015b763bc6900b295efebe71560b44ab281
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2024-03-26 17:18:13 +0530

    migration  II

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,104 +1,134 @@
-import {BsSearch,BsWechat} from "react-icons/bs"
-import {Link, useNavigate} from 'react-router-dom'
-import { useContext, useState } from 'react';
-import Logo from "../../assets/de.png"
-import {FaUserAlt} from 'react-icons/fa'
-import {AiFillSetting,AiFillHeart} from 'react-icons/ai'
-import {LuLogOut} from 'react-icons/lu'
-import "./Nav.css"
+import { BsWechat } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import Logo from "../../assets/de.png";
+import { FaUserAlt } from "react-icons/fa";
+import { AiFillSetting, AiFillHeart } from "react-icons/ai";
+import { LuLogOut } from "react-icons/lu";
 import { Auth } from "../../Provider/AuthProvider";
 import signIn from "../../utils/signIn";
 import signout from "../../utils/signOut";
 const Nav = () => {
-    const {currentUser}=useContext(Auth)
-    const [search,setSearch]=useState("")
-    const[showSearchbar,setShowSearchbar]=useState(false)
-    const [navOpen,setNavOpen]=useState(false)
-    const navigate=useNavigate();
-    const imgStyle={
-        position:"absolute",
-        width:"100%",
-        height:"100%",
-        objectFit:"cover"
-    }
-    const handleSignIn=async()=>{
-        const res= await signIn();
-    }
-    const handleSearch=(e)=>{
-        navigate(`/search/${search}`)
-    }
-  return (
-    <div>
-    <header className="navbar">
-            <Link to={"/"}>
-            <div className="logo-wrapper">
-                <img
-                    src={Logo}
-                    style={imgStyle}
-                    alt='user Image'
-                />
-            </div></Link>
-            <div className="search-box">
-                <input type="text" name="" value={search} onKeyDown={e=>{
-                    if(e.key=="Enter"){
-                        handleSearch(e);
-                    }
-                }}id="" className='' placeholder='Search Anime' onChange={e=>setSearch(e.target.value)}/>
-                <BsSearch className='search-btn' onClick={(e)=>handleSearch(e)}/>
-            </div>
-            <div className='wrapper'>
-            <BsSearch className='search-icon__low-resolution' onClick={()=>{setShowSearchbar(!showSearchbar)}}/>
-            {
-                currentUser!==null?(
-                    <>
-                    <Link to={'/community'}>
-                        <BsWechat className='community'/>
-                        </Link>
-                    <div className='profile-photo-wrapper'onClick={()=>{setNavOpen(!navOpen)}}>
-                        <img
-                            src={currentUser?.photoURL}
-                            alt='user Image'
-                            style={imgStyle}
-                            className='profile-photo'
-                            referrerPolicy="no-referrer"
-                            />
-                    </div>
-                    {
-                        navOpen &&
-                        <div className='profile-options'>
-                            <span>
-                                {currentUser?.displayName}
-                            </span>
-                            <span>{currentUser.email}</span>
-                            <ol>
-                                <li className="option" onClick={()=>{navigate(`/user/`)}}><FaUserAlt/>Profile</li>
-                                <li className="option" onClick={()=>{navigate(`/user/${currentUser?.uid}/watchlist`)}}><AiFillHeart/>WatchList</li>
-                                <li className="option"><AiFillSetting/>Settings</li>
-                                <li className="option" onClick={async()=>{await signout()}}><LuLogOut/>Logout</li>
-                            </ol>
-                        </div>
-                    }
-                    </>
-                ):(
-                    <div className='community__signin__wrapper'>
-                        
-                        
-                <button type="" className='signin-btn' onClick={handleSignIn}>Sign In</button>
-                    </div>
-                    ) 
-            }
-            </div>
-    </header>
-    <div className={`${showSearchbar?'':'remove__from__dom'} search-box__low-resolution`}>
-                <input type="text" name="" value={search} onKeyDown={e=>{
-                    if(e.key=="Enter"){
-                        handleSearch(e);
-                    }
-                }}id="" className='' placeholder='Search Anime' onChange={e=>setSearch(e.target.value)}/>
-                <BsSearch className='search-btn' onClick={(e)=>handleSearch(e)}/>
-            </div>
-    </div>
-  )
-}
+  const { currentUser } = useContext(Auth);
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-export default Nav
+  const handleSignIn = async () => {
+    await signIn();
+  };
+  const handleSearch = () => {
+    navigate(`/search/${search}`);
+  };
+  return (
+    <nav className="h-16 flex bg-base-200 items-center gap-2 justify-between px-2">
+      <Link to={"/"}>
+        <div className="w-16 aspect-square relative">
+          <img
+            src={Logo}
+            className="object-cover absolute w-full h-full"
+            alt="user Image"
+          />
+        </div>
+      </Link>
+
+      <label className="input input-bordered flex items-center gap-2 grow max-w-4xl">
+        <input
+          type="text"
+          className="grow"
+          placeholder="Search by  Anime name"
+          onKeyDown={(e) => {
+            if (e.key == "Enter") {
+              handleSearch(e);
+            }
+          }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="w-4 h-4 opacity-70"
+          onClick={(e) => handleSearch(e)}
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </label>
+
+      {currentUser !== null ? (
+        <div className="flex gap-2 relative">
+          <Link to={"/community"}>
+            <BsWechat size={45} />
+          </Link>
+          <div className="dropdown dropdown-bottom dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn relative w-12 h-12 rounded-full overflow-hidden"
+            >
+              <img
+                src={currentUser?.photoURL}
+                alt="user Image"
+                className="object-cover absolute w-full h-full rounded-lg"
+                referrerPolicy="no-referrer"
+              />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li
+                className="py-2  flex items-center gap-2 cursor-pointer "
+                onClick={() => {
+                  navigate(`/user/`);
+                }}
+              >
+                <span>
+                  <FaUserAlt />
+                  Profile
+                </span>
+              </li>
+              <li
+                className="py-2 px-4 flex items-center gap-2 cursor-pointer "
+                onClick={() => {
+                  navigate(`/user/${currentUser?.uid}/watchlist`);
+                }}
+              >
+                <span>
+                  <AiFillHeart className="" />
+                  WatchList
+                </span>
+              </li>
+              <li className="py-2 px-4 flex items-center gap-2 cursor-pointer ">
+                <span>
+                  <AiFillSetting className="" />
+                  Settings
+                </span>
+              </li>
+              <li
+                className="py-2 px-4 flex items-center gap-2 cursor-pointer "
+                onClick={async () => {
+                  await signout();
+                }}
+              >
+                <span>
+                  <LuLogOut className="" />
+                  Logout
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button className="btn btn-active btn-primary" onClick={handleSignIn}>
+          Sign In
+        </button>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
