612ba5b272a5f0ef313e2ee9730bedd2e100918a
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2024-04-26 20:03:19 +0530

    cart loader

---
+++ client/src/components/Loaders/CartLoader/CartLoader.jsx
@@ -0,0 +1,13 @@
+const cartLoader = () => {
+  return (
+    <>
+      <div className="relative w-40 h-60 md:w-60 md:h-[21rem] p-2 cursor-pointer skeleton"></div>
+      <div className="relative w-40 h-60 md:w-60 md:h-[21rem] p-2 cursor-pointer skeleton"></div>
+      <div className="relative w-40 h-60 md:w-60 md:h-[21rem] p-2 cursor-pointer skeleton"></div>
+      <div className="relative w-40 h-60 md:w-60 md:h-[21rem] p-2 cursor-pointer skeleton"></div>
+      <div className="relative w-40 h-60 md:w-60 md:h-[21rem] p-2 cursor-pointer skeleton"></div>
+    </>
+  );
+};
+
+export default cartLoader;
